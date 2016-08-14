6cdde9b088e42045ee207088c20a67e4b20bd618
clair13 <theclairvoyant1313@yahoo.gr>
2016-08-14 13:02:51 +0200

    Add routes and devise setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  root to: 'visitors#index'
+  
+  devise_for :users
+
+  resources :teachers do
+  	get :subjects
+  end
+
   resources :students do
     get :subjects
   end
